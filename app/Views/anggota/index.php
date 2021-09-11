5472932a637146d81aa810c42640b5b37c390604
rohimoz28 <rohimoz28@gmail.com>
2021-09-11 11:47:14 +0700

    first blood

---
+++ app/Views/anggota/index.php
@@ -0,0 +1,5 @@
+<?= $this->extend('layout/master-layout') ?>
+
+<?php $this->section('content') ?>
+<h2>ini halaman anggota</h2>
+<?php $this->endSection() ?>
---
+++ app/Views/home/index.php
@@ -0,0 +1,7 @@
+<?= $this->extend('layout/master-layout') ?>
+
+<?= $this->section('content') ?>
+
+<h2>Ini halaman Home</h2>
+
+<?= $this->endSection() ?>
