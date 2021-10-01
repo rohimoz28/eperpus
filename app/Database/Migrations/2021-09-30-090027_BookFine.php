d99adf35ea2b6306d4b6144ff37f4ef1794bcf10
rohimoz28 <rohimoz28@gmail.com>
2021-10-01 19:37:11 +0700

    buat migrasi&seeder bookfine

--- app/Database/Migrations/2021-09-30-090027_BookFine.php
+++ app/Database/Migrations/2021-09-30-090027_BookFine.php
@@ -8,11 +8,30 @@ class BookFine extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'book_fine_id' => [
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => TRUE,
+                'auto_increment' => TRUE
+            ],
+            'description' => [
+                'type' => 'VARCHAR',
+                'constraint' => 50
+            ],
+            'book_fine' => [
+                'type' => 'INT',
+                'constraint' => 10
+            ],
+            'created_at datetime default current_timestamp',
+            'updated_at datetime default current_timestamp on update current_timestamp'
+        ]);
+        $this->forge->addKey('book_fine_id', true);
+        $this->forge->createTable('book_fines');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('book_fines');
     }
 }
---
+++ app/Database/Seeds/BookFineSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+use CodeIgniter\I18n\Time;
+
+class BookFineSeeder extends Seeder
+{
+    public function run()
+    {
+        $data = [
+            'description' => 'Rusak',
+            'book_fine' => 30000,
+            'created_at' => Time::now()
+        ];
+
+        $this->db->table('book_fines')->insert($data);
+    }
+}
